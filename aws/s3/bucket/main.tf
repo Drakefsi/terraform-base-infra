b6ec88aadff54d3a8334963ab51df37d00cfcac8
Pablo Villaruel <pablolibo@gmail.com>
2022-03-16 19:28:00 -0300

    modificado:     s3/bucket/main.tf 	modificado:     s3/website/main.tf

--- aws/s3/bucket/main.tf
+++ aws/s3/bucket/main.tf
@@ -1,5 +1,13 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-  acl    = var.acl
-  policy = var.policy
+}
+
+resource "aws_s3_bucket_policy" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = var.acl
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = var.policy
 }
--- aws/s3/website/main.tf
+++ aws/s3/website/main.tf
@@ -1,9 +1,29 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-  acl    = "public-read"
-
+  bucket        = var.bucket_name
   force_destroy = true
 
+  website {
+    index_document = var.index_document
+    error_document = var.error_document
+  }
+
+
+}
+
+resource "aws_s3_bucket_cors_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    expose_headers  = ["Authorization"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
   policy = <<EOF
 {
     "Version": "2008-10-17",
@@ -20,17 +40,9 @@ resource "aws_s3_bucket" "bucket" {
     ]
 }
 EOF
+}
 
-  website {
-    index_document = var.index_document
-    error_document = var.error_document
-  }
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET"]
-    allowed_origins = ["*"]
-    expose_headers  = ["Authorization"]
-    max_age_seconds = 3000
-  }
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
 }
