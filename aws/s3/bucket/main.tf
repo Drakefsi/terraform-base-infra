998c425fb1427bb0c87aa4d39431c78b20b09acc
Pablo Villaruel <pablolibo@gmail.com>
2022-03-17 11:42:43 -0300

    modificado:     bucket/main.tf

--- aws/s3/bucket/main.tf
+++ aws/s3/bucket/main.tf
@@ -4,10 +4,10 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_policy" "bucket" {
   bucket = aws_s3_bucket.bucket.id
-  policy = var.acl
+  policy = var.policy
 }
 
 resource "aws_s3_bucket_acl" "bucket" {
   bucket = aws_s3_bucket.bucket.id
-  acl    = var.policy
+  acl    = var.acl
 }
